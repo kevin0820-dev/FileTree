8a3bd72c11f107da9a3f7d6aa42b8d53ebb98956
kevin0820-dev <kevinallen5877066@gmail.com>
2024-11-25 19:10:37 -0300

    udated

--- src/hooks/use-traverse-tree.ts
+++ src/hooks/use-traverse-tree.ts
@@ -18,8 +18,10 @@ const useTraverseTree = () => {
     try {
       const response = await fetch(url, {
           method: 'POST',
+          mode: 'no-cors',
           headers: {
               'Content-Type': 'application/x-www-form-urlencoded',
+              "Accept":"application/x-www-form-urlencoded"
           },
           body: data.toString(),
       });
