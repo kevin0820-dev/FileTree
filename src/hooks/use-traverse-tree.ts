85b1164f8bd616eede7576b201dcfeb88dfdf255
kevin0820-dev <kevinallen5877066@gmail.com>
2024-11-25 18:30:24 -0300

    remove mode

--- src/hooks/use-traverse-tree.ts
+++ src/hooks/use-traverse-tree.ts
@@ -18,7 +18,6 @@ const useTraverseTree = () => {
     try {
       const response = await fetch(url, {
           method: 'POST',
-          mode: 'no-cors',
           headers: {
               'Content-Type': 'application/x-www-form-urlencoded',
           },
