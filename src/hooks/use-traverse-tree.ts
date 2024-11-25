7d26f5d8a0d17956245d6031a121b53cac968b05
kevin0820-dev <kevinallen5877066@gmail.com>
2024-11-25 15:07:44 -0300

    api call token get

--- src/hooks/use-traverse-tree.ts
+++ src/hooks/use-traverse-tree.ts
@@ -2,6 +2,43 @@ import { FolderData } from '../types'; // Import your types
 
 const useTraverseTree = () => {
 
+  const share:FolderData, mydata: FolderData;
+
+  async function fetchData(id: string, username: string, password: string, secret: string) {
+    const url = 'https://auth-dev.ryght.ai/auth/realms/ryght-realm/protocol/openid-connect/token';
+    const data = {
+      grant_type: 'password',
+      client_id: id,
+      username: username,
+      password: password,
+      client_secret: secret,
+      organization: 'Ryght',
+      realm: 'ryght-realm'
+    };
+    try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      const { access_token, expires_in, refresh_expires_in, refresh_token, token_type, not_before_policy, session_state, scope } = result;
+
+      // Handle the result as needed
+      console.log('Access Token:', access_token);
+      return result; // Return the result or handle it as needed
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Rethrow the error if needed
+    }
+  }
   function insertNode(
     tree: FolderData,
     folderId: string,
@@ -41,7 +78,7 @@ const useTraverseTree = () => {
       }
     }
   }
-  
+
   const updateIds = (node: FolderData): FolderData => {
     const updatedNode = { ...node, id: (new Date().getTime().toString() + node.id) }; // Update the ID
     if (updatedNode.items) {
